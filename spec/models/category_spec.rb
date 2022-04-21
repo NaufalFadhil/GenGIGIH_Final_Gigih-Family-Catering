5d757f45997b8bff507e1222ae5fa384762e429c
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-04-21 13:33:08 +0000

    Create test for Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+   describe "Factory test" do
+    it "has a valid factory" do
+      expect(FactoryBot.build(:category)).to be_valid
+    end
+  end
+
+  describe "Field test" do
+    it 'is valid with a name' do
+      expect(FactoryBot.build(:category)).to be_valid
+    end
+    
+    it 'is invalid without a name' do
+      category = FactoryBot.build(:category, name: nil)
+      category.valid?
+      
+      expect(category.errors[:name]).to include("can't be blank")
+    end
+  end
 end
