33633f3e503304a84ef66b877ec14562d835d82f
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-04-23 02:36:58 +0000

    rm: Unused method in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
   has_many :order_detail
   belongs_to :customer, foreign_key: 'customer_id'
   belongs_to :admin, foreign_key: 'admin_id'
-
-  def self.get_customer_orders
-    where("name LIKE ?", "#{letter}%").order(:name)
-  end
 end
